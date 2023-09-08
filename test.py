504015a9d91a819bb350456188d4ac6ad45b88fd
2218069 <amno0334a@naver.com>
2023-09-08 16:36:54 +0900

    2023.09.08_Quiz
    1주차 컴퓨터 프로그래밍 과제 연산자 입력하기

---
+++ test.py
@@ -0,0 +1,10 @@
+number = input("숫자를 입력하세요")
+print(number)
+
+name = input("이름을 입력하세요")
+age = input("나이를 입력하세요")
+print("이름은", name, "나이는", age)
+
+a = input("숫자를 입력하세요")
+b = input("숫자를 입력하세요")
+print("입력하신 두 숫자는", a, b)
